2d20b60e4740eb34823bb2acd7ceb5780dadf74a
Philippe Casgrain <github@casgrain.com>
2013-01-07 21:28:39 -0500

    Fixed typo in comment

--- CCMenu/Classes/CCMGrowlAdaptor.m
+++ CCMenu/Classes/CCMGrowlAdaptor.m
@@ -17,19 +17,19 @@ struct {
 {
 	growlNotifications[0].key = CCMSuccessfulBuild;
 	growlNotifications[0].name = NSLocalizedString(@"Build successful", "Growl notification for successful build");
-	growlNotifications[0].description = NSLocalizedString(@"Yet another successful build!", "For Growl notificiation");
+	growlNotifications[0].description = NSLocalizedString(@"Yet another successful build!", "For Growl notification");
 
 	growlNotifications[1].key = CCMStillFailingBuild;
 	growlNotifications[1].name = NSLocalizedString(@"Build still failing", "Growl notification for successful build");
-	growlNotifications[1].description = NSLocalizedString(@"The build is still broken.", "For Growl notificiation");
+	growlNotifications[1].description = NSLocalizedString(@"The build is still broken.", "For Growl notification");
 	
 	growlNotifications[2].key = CCMBrokenBuild;
 	growlNotifications[2].name = NSLocalizedString(@"Broken build", "Growl notification for successful build");
-	growlNotifications[2].description = NSLocalizedString(@"Recent checkins have broken the build.", "For Growl notificiation");
+	growlNotifications[2].description = NSLocalizedString(@"Recent checkins have broken the build.", "For Growl notification");
 
 	growlNotifications[3].key = CCMFixedBuild;
 	growlNotifications[3].name = NSLocalizedString(@"Fixed build", "Growl notification for successful build");
-	growlNotifications[3].description = NSLocalizedString(@"Recent checkins have fixed the build.", "For Growl notificiation");
+	growlNotifications[3].description = NSLocalizedString(@"Recent checkins have fixed the build.", "For Growl notification");
 }	
 
 - (NSDictionary *)registrationDictionaryForGrowl
